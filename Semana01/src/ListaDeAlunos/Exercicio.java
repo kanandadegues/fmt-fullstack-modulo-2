68d9b4a4a9e5787deb75e61f87fb6914e0bcbea3
Kananda Degues <kananda.degues@gmail.com>
2023-05-13 15:49:54 -0300

    Ex 4 - Lista de alunos

--- Semana01/src/ListaDeAlunos/Exercicio.java
+++ Semana01/src/ListaDeAlunos/Exercicio.java
@@ -1,4 +1,22 @@
 package ListaDeAlunos;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exercicio {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> listaAlunos = new ArrayList<>();
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno(a) ou fim para finalizar");
+        String nomeDigitado = entrada.next();
+
+        while(!nomeDigitado.equalsIgnoreCase("fim")){
+            System.out.println("Digite nome do aluno ou fim para finalizar");
+            nomeDigitado = entrada.next();
+        }
+
+    }
 }

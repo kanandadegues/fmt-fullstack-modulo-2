998c66217c4d32ae93faf89cb443c5b73f9b391d
Kananda Degues <kananda.degues@gmail.com>
2023-05-13 16:02:32 -0300

    Ex 6 - Receba 3 notas e armazene em um Array

--- Semana01/src/ListaDeAlunos/Exercicio.java
+++ Semana01/src/ListaDeAlunos/Exercicio.java
@@ -8,12 +8,27 @@ public class Exercicio {
     public static void main(String[] args) {
 
         ArrayList<String> listaAlunos = new ArrayList<>();
+        ArrayList<double[]> listaNotas = new ArrayList<>();
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite o nome do aluno(a) ou fim para finalizar");
         String nomeDigitado = entrada.next();
 
         while(!nomeDigitado.equalsIgnoreCase("fim")){
+
+            System.out.println("Digite as notas do aluno(a)" + nomeDigitado);
+            System.out.println("Digite a primeira nota = ");
+            double n1 = entrada.nextDouble();
+            System.out.println("Digite a segunda nota = ");
+            double n2 = entrada.nextDouble();
+            System.out.println("Digite a terceira nota = ");
+            double n3 = entrada.nextDouble();
+
+            double[] notas = new double[3];
+            notas[0] = n1;
+            notas[1] = n2;
+            notas[2] = n3;
+
             System.out.println("Digite nome do aluno ou fim para finalizar");
             nomeDigitado = entrada.next();
         }

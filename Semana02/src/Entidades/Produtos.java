0280ba4bf954577ac282361eb8f9b993709387a8
Kananda Degues <kananda.degues@gmail.com>
2023-05-14 16:28:10 -0300

    Criação de um construtor para a classe produtos

--- Semana02/src/Entidades/Produtos.java
+++ Semana02/src/Entidades/Produtos.java
@@ -4,4 +4,10 @@ public class Produtos {
     private String nomeProduto;
     private double precoProduto;
     private int quantidadeProduto;
+
+    public Produtos(String nomeProduto, double precoProduto, int quantidadeProduto) {
+        this.nomeProduto = nomeProduto;
+        this.precoProduto = precoProduto;
+        this.quantidadeProduto = quantidadeProduto;
+    }
 }
--- Semana02/src/Main.java
+++ Semana02/src/Main.java
@@ -3,6 +3,6 @@ import Entidades.Produtos;
 public class Main {
     public static void main(String[] args) {
 
-        Produtos produto = new Produtos();
+        Produtos produto = new Produtos("Celular", 2500.00, 20);
     }
 }

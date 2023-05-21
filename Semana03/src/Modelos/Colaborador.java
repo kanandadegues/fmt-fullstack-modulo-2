f74a059276f3117d90308a233611b3f352b2cbf7
Kananda Degues <kananda.degues@gmail.com>
2023-05-21 15:00:43 -0300

    Ex 3 - Criação classe colaborador

---
+++ Semana03/src/Modelos/Colaborador.java
@@ -0,0 +1,58 @@
+package Modelos;
+
+import java.util.Date;
+
+public class Colaborador {
+    private String nome;
+    private Date dtAdmissao;
+    private Date dtDesligamento;
+    private Cargo cargo;
+    private double salario;
+
+    public Colaborador(String nome, Date dtAdmissao, Cargo cargo) {
+        this.nome = nome;
+        this.dtAdmissao = dtAdmissao;
+        this.cargo = cargo;
+        this.salario = cargo.getSalarioBase();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDtAdmissao() {
+        return dtAdmissao;
+    }
+
+    public void setDtAdmissao(Date dtAdmissao) {
+        this.dtAdmissao = dtAdmissao;
+    }
+
+    public Date getDtDesligamento() {
+        return dtDesligamento;
+    }
+
+    public void setDtDesligamento(Date dtDesligamento) {
+        this.dtDesligamento = dtDesligamento;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}

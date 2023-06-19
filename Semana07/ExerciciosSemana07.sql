c23938cd019326d0edf2d0cd28c4dd22161f8988
Kananda Degues <kananda.degues@gmail.com>
2023-06-19 13:14:06 -0300

    Ex 1

---
+++ Semana07/ExerciciosSemana07.sql
@@ -0,0 +1,24 @@
+-- Ex 1 - Crie uma Tabela chamada clientes
+
+CREATE TABLE CLIENTES (
+		ID INT,
+		NOME VARCHAR(100),
+		TELEFONE VARCHAR(20),
+		ENDERECO VARCHAR(150),
+)
+
+-- Ex 2 - Crie um Script para inserir 3 Clientes na tabela
+
+-- Ex 3 - Crie um script que selecione todos os clientes
+
+-- Ex 4 - Selecione os clientes por Id
+
+-- Ex 5 - Selecione os clientes por nome Utilizando Like
+
+-- Ex 6 - Crie um Script para atualizar um cliente
+
+-- Ex 7 - Crie o Script para excluir um cliente
+
+-- Ex 8 - Normalize a tabela funcionarios até sua terceira forma normal
+
+-- EX 9 - Crie um script para selecionar os dados da estrutura criada no exercício 8

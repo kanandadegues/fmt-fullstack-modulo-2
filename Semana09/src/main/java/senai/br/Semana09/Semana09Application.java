e74b85742475b2ae403dff6c1a64dd4ef1f6e0eb
Kananda Degues <kananda.degues@gmail.com>
2023-07-31 19:46:55 -0300

    Ex 1-10

---
+++ Semana09/src/main/java/senai/br/Semana09/Semana09Application.java
@@ -0,0 +1,13 @@
+package senai.br.Semana09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana09Application.class, args);
+	}
+
+}
---
+++ Semana09/src/test/java/senai/br/Semana09/Semana09ApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.br.Semana09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
